b01a076153925aa385961bc57b5ba0d316746b82
Parghi Nikunj <nikunj9803@gmail.com>
2026-08-22 10:11:06 +0530

    Add files via upload

---
+++ binary_operator.cpp
@@ -0,0 +1,33 @@
+//binary_operator_overloading
+#include<iostream>
+using namespace std;
+class binary_example{
+    public: int a;
+    public:binary_example(){
+
+    }
+    public:binary_example(int x){
+        a=x;
+
+    }
+    public:void show(){
+        cout<<"addition of object"<<a<<endl;
+    }
+    binary_example operator +(binary_example o1){
+        //(temp=a(data_member)+b(object as argument);
+        binary_example temp;
+        temp.a=a+o1.a;
+        return temp;
+
+    }
+};
+int main(){
+    binary_example obj(67);
+    binary_example obj1(90);
+    binary_example obj2();
+    obj2=obj+obj1;
+    obj2.show();
+    return 0;
+
+
+}
